0a0877dea2c268d30d8a5e72665ebd667359d509
Domenico Amato <domen.amato91@gmail.com>
2016-07-24 12:49:16 +0200

    -Cleaned Service Code

--- app/src/main/java/com/example/domen/mymanga/Service/MangaDetailService.java
+++ app/src/main/java/com/example/domen/mymanga/Service/MangaDetailService.java
@@ -8,6 +8,7 @@ import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.example.domen.mymanga.Activity.AllMangaActivity;
 import com.example.domen.mymanga.Models.Contract;
 import com.example.domen.mymanga.R;
 
@@ -31,9 +32,15 @@ public class MangaDetailService extends IntentService {
 
     @Override
     protected void onHandleIntent(Intent intent) {
-
+        /*
+        *   Recupero l'id del manga dall'intent
+        */
         String id = intent.getStringExtra(Contract.Manga.COLUMN_MANGA_ID);
         String result;
+
+        /*
+        *   Scarico un JSON tramite API dal sito mangaeden
+        */
         try {
             String stringUrl = getBaseContext().getString(R.string.mangaeden_api_detail)+id;
             Log.v("MyMangaService", "Url is "+stringUrl);
@@ -41,7 +48,10 @@ public class MangaDetailService extends IntentService {
             NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
             if (networkInfo != null && networkInfo.isConnected()) {
                 result = downloadUrl(stringUrl);
-                //Log.v("MyMangaService", result);
+
+                /*
+                *   Invio i risultati tramite Intent ad un Broadcast Receiver
+                */
                 Intent resultBroadcast = new Intent();
                 resultBroadcast.setAction(getApplicationContext().getString(R.string.broadcast_action));
                 Bundle extraBundle = new Bundle();
@@ -49,56 +59,39 @@ public class MangaDetailService extends IntentService {
                 resultBroadcast.putExtra("myExtraBundle", extraBundle);
                 resultBroadcast.putExtra(Contract.Manga.COLUMN_MANGA_ID, id);
                 sendBroadcast(resultBroadcast);
-
             }
         }catch (Exception e){
             Log.e("MyMangaService", e.getMessage());
         }
     }
 
+    /*
+    *   Metodo usato per il download dei dati dall'url passata come parametro
+    */
     private String downloadUrl(String myurl) throws IOException {
         InputStream is = null;
-        // Only display the first 500 characters of the retrieved
-        // web page content.
-        int len = 500;
 
         try {
+
             URL url = new URL(myurl);
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setReadTimeout(10000 /* milliseconds */);
-            conn.setConnectTimeout(15000 /* milliseconds */);
+            conn.setReadTimeout(10000);
+            conn.setConnectTimeout(15000);
             conn.setRequestMethod("GET");
             conn.setDoInput(true);
-            // Starts the query
             conn.connect();
+
             int response = conn.getResponseCode();
             Log.d("MyMangaService", "The response is: " + response);
             is = conn.getInputStream();
 
-            // Convert the InputStream into a string
-            String contentAsString = readIt(is);
+            String contentAsString = AllMangaActivity.readIt(is);
             return contentAsString;
 
-            // Makes sure that the InputStream is closed after the app is
-            // finished using it.
         } finally {
             if (is != null) {
                 is.close();
             }
         }
-    }
-
-    public String readIt(InputStream stream) throws IOException, UnsupportedEncodingException {
-
-        Reader reader = null;
-        BufferedReader rd = new BufferedReader(new InputStreamReader(stream));
-        String line;
-        StringBuffer response = new StringBuffer();
-        while((line = rd.readLine()) != null) {
-            response.append(line);
-            response.append('\r');
-        }
-        rd.close();
-        return response.toString();
     }
 }
